aea6d31e36244ebf896542bd476d83bf83827e3a
tylerwill <twill4398@gmail.com>
2021-05-26 18:44:31 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
